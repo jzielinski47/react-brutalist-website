2d21677e3ad0d1769004da7767a63d2d724da946
Jakub Zieliński <jzielinski47dev@gmail.com>
2023-02-27 12:49:43 +0100

    content-fill

--- src/assets/content.ts
+++ src/assets/content.ts
@@ -4,6 +4,10 @@ import texture2 from './gfx/texture2.png';
 import image1 from './gfx/image1.png';
 import image2 from './gfx/image2.png';
 
+import hotelforum from './gfx/hotelforum.png';
+import thenationaltheatre from './gfx/thenationaltheatre.png';
+import embassyofczechoslovakia from './gfx/embassyofczechoslovakia.png';
+
 export const CONTENT = {
     header: {
         title: 'Brutalism',
@@ -41,26 +45,31 @@ export const CONTENT = {
             image: image2
         },
         {
-            title: 'Key elements',
-            description: 'The key elements of Brutalism include large, modular forms; plain surfaces; and the predominance of exposed concrete, which is often used in bold geometric shapes. Other materials such as brick, steel, and glass may also be used.',
+            title: 'Post war era',
+            description: 'Brutalism was popular in post-war Europe, and is still used today for public and institutional buildings. It is often used for educational, governmental, and religious buildings, as it conveys a sense of solidity and permanence. It is also gaining popularity in residential architecture, as homeowners appreciate its modern, minimalist aesthetic.',
             image: image1
         },
         {
-            title: 'Key elements',
-            description: 'The key elements of Brutalism include large, modular forms; plain surfaces; and the predominance of exposed concrete, which is often used in bold geometric shapes. Other materials such as brick, steel, and glass may also be used.',
+            title: 'Web Brutalism',
+            description: 'Web Brutalism is a relatively new trend in web design that has been gaining traction in recent years. This style of design is characterized by its rough, raw aesthetic with heavy use of text, basic shapes, and a minimalistic approach. Web Brutalism eschews the sleek, polished look of modern web design in favor of a more edgy and industrial feel.',
             image: image1
         }
     ],
     examples: [
         {
-            title: 'Key elements',
+            title: 'Hotel forum, Krakow',
             description: 'The key elements of Brutalism include large, modular forms; plain surfaces; and the predominance of exposed concrete, which is often used in bold geometric shapes. Other materials such as brick, steel, and glass may also be used.',
-            image: image1
+            image: hotelforum
         },
         {
-            title: 'World war influence',
+            title: 'The National Theatre, London',
             description: 'Europe was heavily affected by World War II and its architecture was greatly impacted. Many cities and towns across Europe were destroyed or heavily damaged from bombing and warfare. In the immediate aftermath of the war, Europe saw a drastic change in its architecture. Cities had to rebuild and many temporary structures were built in place of the destroyed buildings.',
-            image: image2
+            image: thenationaltheatre
+        },
+        {
+            title: 'Embassy of Czechoslovakia, Berlin',
+            description: 'Europe was heavily affected by World War II and its architecture was greatly impacted. Many cities and towns across Europe were destroyed or heavily damaged from bombing and warfare. In the immediate aftermath of the war, Europe saw a drastic change in its architecture. Cities had to rebuild and many temporary structures were built in place of the destroyed buildings.',
+            image: embassyofczechoslovakia
         },
     ],
     footer: {
