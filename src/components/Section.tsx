2aeaa36eac91fb4a2ab5a19385439cbe0acd4036
Jakub Zieliński <jzielinski47dev@gmail.com>
2023-02-28 09:40:47 +0100

    code-refactor

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -3,8 +3,6 @@ import React from 'react'
 interface ISection { title: string; description: string; image: string; index: number; }
 
 const Section = ({ title, description, image, index }: ISection) => {
-
-
     return (
         <div className='section' style={index % 2 == 0 ? { flexDirection: 'row-reverse' } : { flexDirection: 'row' }}>
             <div className='textWrapper'>
