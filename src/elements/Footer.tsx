11c7a7193ec5593e3b76ef38797ea4bc90f9db4c
Jakub Zieliński <jzielinski47dev@gmail.com>
2023-03-07 15:28:11 +0100

    footer-link-update

--- src/elements/Footer.tsx
+++ src/elements/Footer.tsx
@@ -5,7 +5,7 @@ import { CONTENT as content } from '../assets/content';
 const Footer = () => {
     return (
         <footer>
-            <Link to="https://github.com/jzielinski47">{content.footer.content}</Link>
+            <Link to="https://github.com/jzielinski47"><h2>{content.footer.content}</h2></Link>
         </footer>
     )
 }
