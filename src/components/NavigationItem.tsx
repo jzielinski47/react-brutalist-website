17f19c48088d79a997c17becab27155983357db9
Jakub Zieliński <jzielinski47dev@gmail.com>
2023-03-07 14:00:42 +0100

    navigation-item-update

---
+++ src/components/NavigationItem.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavigationItem = ({ title, href }: { title: string; href: string; }) => {
+    return (
+        <li>
+            <h2>{title}</h2>
+        </li>
+    )
+}
+
+export default NavigationItem
--- src/elements/Header.tsx
+++ src/elements/Header.tsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { motion } from 'framer-motion';
+import NavigationItem from '../components/NavigationItem';
 
 const Header = ({ title, navigation }: { title: string; navigation: any[] }) => {
+
+    useEffect(() => {
+
+    });
+
     return (
         //  <motion.header animate={{ scale: 1 }} initial={{ scale: 0 }}>
         <motion.header >
             <h2>{title}</h2>
             <nav>
-                {navigation.map((nav: { title: string }, i: number) => (
-                    <li key={i}>
-                        <h2>{nav.title}</h2>
-                    </li>
-                ))}
+
+
+                {navigation.map((nav: { title: string; href: string; }, i: number) => <NavigationItem key={i} title={nav.title} href={nav.href} />)}
+
             </nav>
         </motion.header>
     )
